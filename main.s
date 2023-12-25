c825adde93bf21ffc423c1abe2ae11fb3bac1482
panagiotisppg <panagot94@gmail.com>
2023-12-25 14:18:40 +0200

    part A done? commented lines with reasons of commment will be deleted

--- main.s
+++ main.s
@@ -18,6 +18,10 @@
 main:
 #--------------------------------main-----------------------------------
     loop:
+        li   $a0, '\n'
+        li   $v0, 11
+        syscall
+
         jal readOption
         
         #while((op>=1) && (op<=8))
@@ -45,7 +49,7 @@ main:
 
         la   $a0, pinA
         li   $a1, 10
-        addi $sp, $sp, -4         #saves off the $ra
+        addi $sp, $sp, -4          #saves off the $ra
         sw   $ra, 0($sp)           #save return address
 
         jal readPin
@@ -53,7 +57,7 @@ main:
         lw   $ra, 0($sp)       
         addi $sp, $sp, 4      
 
-        jr $ra
+        # jr $ra # removed cuz was making infinite loop cuz it had option selected and jumped to line 26(basically 29)
 
         j loop
 
@@ -61,7 +65,7 @@ main:
 
         #op=2
         beq  $v1, 2, ReadingArrayB
-        bne  $v1, 1, not_2
+        bne  $v1, 2, not_2
     ReadingArrayB:
         #System.out.println("Reading Array B");
         la   $a0, Reading_Array_B 
@@ -72,7 +76,7 @@ main:
 
         la   $a0, pinB
         li   $a1, 10
-        addi $sp, $sp, -4         #saves off the $ra
+        addi $sp, $sp, -4          #saves off the $ra
         sw   $ra, 0($sp)           #save return address
 
         jal readPin
@@ -80,7 +84,7 @@ main:
         lw   $ra, 0($sp)       
         addi $sp, $sp, 4      
 
-        jr $ra
+        # jr $ra
 
         j loop
 
@@ -182,40 +186,42 @@ main:
         syscall
 
         addi $v1,  $v0,  0
+
+        la $a0, line
+        li $v0, 4
+        syscall
         
         jr   $ra
 
     #-----------------------------readPin(int [] pin)------------------------------
     readPin:
-        
-        lw   $t0, 0        #int i=0;
 
         move $s1, $a0         #preserve address of array
-        move $s2, $a1         #preserve address of elements 
-        
-    continues_in_the_loop:
+        move $s2, $a1         #preserve address of elements
 
-        #System.out.print ("Position " + i +" :");
+        li   $t5, 0           # counter
+
+    input_loop:
+        beq  $t5, 10, exit_input
         la   $a0, Position
         li   $v0, 4
-        syscall
 
-        move $v0, $t0
+        syscall              # print("Position [")
+        move $a0, $t5
         li   $v0, 1
-        syscall
-
+        syscall              # print(counter)
         la   $a0, colon
         li   $v0, 4
+        syscall              # print("] :")
+        li   $v0, 5          # get input number
         syscall
-        
-        li   $v0, 5        #in.nextInt();
-        syscall
-        
-        sw   $v0, 0($s1)        #store the element
-        addi $s1, $s1, 4       
-        addi $s2, $s2, -1       
-        add  $t0, $t0, 1
-        bnez $s2, continues_in_the_loop
+
+        sw   $v0, ($s1)      # store inputed value to current address of list
+        addi $s1, 4          # move pointer 4 bytes deeper in the array to store the next element 
+        addi $t5, 1          # nums inputed ++
+        j input_loop         # loop
+
+        exit_input:
 
         jr $ra
 
@@ -226,24 +232,25 @@ main:
     mikosC:                           .word   0
     i:                                .space  4
     op:                               .space  4
-    pinA:                             .space  10
-    pinB:                             .space  10
+    pinA:                             .space  40 // 40 instead of 10 cuz we ant 10 integers so 10*4 bytes
+    pinB:                             .space  40 // 40 instead of 10 cuz we ant 10 integers so 10*4 bytes
     SparseA:                          .space  20
     SparseB:                          .space  20
     SparseC:                          .space  20
+    line:                             .asciiz "-----------------------------\n"
     values:                           .asciiz " values "
-    Reading_Array_A:                  .asciiz "Reading Array A\n"
-    Reading_Array_B:                  .asciiz "Reading Array B \n"
-    Creating_Spare_Array_A:           .asciiz "Creating Sparse Array A \n"
-    Creating_Spare_Array_B:           .asciiz "Creating Sparse Array B \n"
-    Creating_Spare_Array_C:           .asciiz "Creating Sparse Array C \n"
-    Displaying_Sparse_Array_A:        .asciiz "Displaying Sparse Array A \n"
-    Displaying_Sparse_Array_B:        .asciiz "Displaying Sparse Array B \n"
-    Displaying_Sparse_Array_C:        .asciiz "Displaying Sparse Array C \n"
+    Reading_Array_A:                  .asciiz "Reading Array A\n\n"
+    Reading_Array_B:                  .asciiz "Reading Array B\n\n"
+    Creating_Spare_Array_A:           .asciiz "Creating Sparse Array A\n"
+    Creating_Spare_Array_B:           .asciiz "Creating Sparse Array B\n"
+    Creating_Spare_Array_C:           .asciiz "Creating Sparse Array C\n"
+    Displaying_Sparse_Array_A:        .asciiz "Displaying Sparse Array A\n"
+    Displaying_Sparse_Array_B:        .asciiz "Displaying Sparse Array B\n"
+    Displaying_Sparse_Array_C:        .asciiz "Displaying Sparse Array C\n"
     #-----------------readOption-----------------------------------------------
     Choice:                           .asciiz "\n-----------------------------\n1.Read Array A \n2.Read Array B \n3.Create Sparse Array A \n4.Create Sparse Array B \n5.Create Sparse Array C= A + B \n6.Display Sparse Array A \n7.Display Sparse Array B \n8.Display Sparse Array C \n0.Exit\nChoice? "
     #----------------readPin/printSparse----------------------------------------
-    Position:                         .asciiz "Position   "
+    Position:                         .asciiz "Position ["
     Position_with_colon:              .asciiz "Position:   "
     Values:                           .asciiz "Values: "
-    colon:                            .asciiz " :\n"
+    colon:                            .asciiz "] :"
